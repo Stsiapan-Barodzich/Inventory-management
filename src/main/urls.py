a2f367f37c7bd38c90324c019cd1f21cdba4e5de
Stsiapan-Barodzich <stepanborodic@gmail.com>
2025-05-23 23:46:49 +0300

    added models and base crud

---
+++ src/main/urls.py
@@ -0,0 +1,15 @@
+# from django.contrib import admin
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from .views import ProductStockViewSet, ProductViewSet, UserViewSet, WarehouseViewSet
+
+router = DefaultRouter()
+router.register(r"warehouses", WarehouseViewSet)
+router.register(r"products", ProductViewSet)
+router.register(r"product-stocks", ProductStockViewSet)
+router.register(r"users", UserViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
