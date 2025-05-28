095ddead693fc5e085f84204f1268c3f8294c0f4
Stsiapan-Barodzich <stepanborodic@gmail.com>
2025-05-28 14:58:30 +0300

    fixed serializers

--- src/main/serializers.py
+++ src/main/serializers.py
@@ -4,25 +4,25 @@ from rest_framework import serializers
 from .models import Product, ProductStock, Warehouse
 
 
-class UserSerializer(serializers.ModelSerializer[User]):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = "__all__"
 
 
-class ProductSerializer(serializers.ModelSerializer[Product]):
+class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = "__all__"
 
 
-class ProductStockSerializer(serializers.ModelSerializer[ProductStock]):
+class ProductStockSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProductStock
         fields = "__all__"
 
 
-class WarehouseSerializer(serializers.ModelSerializer[Warehouse]):
+class WarehouseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Warehouse
         fields = "__all__"
