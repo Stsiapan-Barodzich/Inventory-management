49fb7282820fabf2ef5ce1ac420caec4b5792556
Stsiapan-Barodzich <stepanborodic@gmail.com>
2025-05-30 13:52:15 +0300

    fixed views and serializers, added absolute paths

--- src/Inventory_management/urls.py
+++ src/Inventory_management/urls.py
@@ -4,6 +4,6 @@ from django.urls import include, path
 # fmt: off
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("main.urls"))
+    path("", include("main.urls")),
 ]
 # fmt: on
--- src/main/serializers.py
+++ src/main/serializers.py
@@ -8,22 +8,25 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = "__all__"
-        read_only_fields = ["id", "date_joined", "last_login"]
+        read_only_fields = ["id", "date_joined", "last_login", "is_staff", "is_superuser", "is_active"]
 
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = "__all__"
+        read_only_fields = ["id"]
 
 
 class ProductStockSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProductStock
         fields = "__all__"
+        read_only_fields = ["id"]
 
 
 class WarehouseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Warehouse
         fields = "__all__"
+        read_only_fields = ["id"]
