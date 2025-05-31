c7ba2ddc197f72f14c96435b0938c94534bd7030
Stsiapan-Barodzich <stepanborodic@gmail.com>
2025-05-31 18:10:50 +0300

    deleted usless imports

--- src/Inventory_management/urls.py
+++ src/Inventory_management/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
 
-# fmt: off
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("main.urls")),
 ]
-# fmt: on
