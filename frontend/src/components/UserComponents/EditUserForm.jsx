f36d6840d9034eaec28602713cd325eb4606191f
Stsiapan-Barodzich <stepanborodic@gmail.com>
2025-06-20 13:58:24 +0300

    fixes after review

--- frontend/src/components/UserComponents/EditUserForm.jsx
+++ frontend/src/components/UserComponents/EditUserForm.jsx
@@ -45,11 +45,11 @@ export default function EditUserForm() {
       });
       console.log("Response received:", response);
       setSuccess("User edited successfully!");
-      setError(""); // Сброс ошибки при успехе
-      // Задержка для отображения сообщения
+      setError(""); 
+      
       setTimeout(() => {
         navigate(`/users/${id}`);
-      }, 1000); // Задержка 1 секунда
+      }, 1000); 
     } catch (err) {
       console.error("API error:", err);
       setError("Error updating user: " + err.message);
